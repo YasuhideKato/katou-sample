065a32dc13d30067655d99300a581cd3994b5de3
YasuhideKato <ya.katou@ntt.com>
2015-11-04 12:55:36 +0900

    new

---
+++ lib/katou/sample.rb
@@ -0,0 +1,7 @@
+require "katou/sample/version"
+
+module Katou
+  module Sample
+    # Your code goes here...
+  end
+end
---
+++ lib/katou/sample/version.rb
@@ -0,0 +1,5 @@
+module Katou
+  module Sample
+    VERSION = "0.1.0"
+  end
+end
